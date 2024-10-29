125695a87b1f0f68b6c8afbf481b568d0b13b24d
Garry Morrison <garry@semantic-db.org>
2024-10-30 10:01:43 +1030

    fixed gitignore

--- src/testing_load_save_chunks.py
+++ src/testing_load_save_chunks.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     NM0 = sf.NeuralModule('module 0')
     source_filename0 = 'machines/greetings.chunk'
-    dest_filename0 = 'machines/tmp_0.chunk'
+    dest_filename0 = 'machines/tmp/0.chunk'
     print(f"loading file: {source_filename0}")
     NM0.load_chunks(source_filename0)
     print(f"saving file: {dest_filename0}")
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     print()
     NM1 = sf.NeuralModule('module 1')
     source_filename1 = 'machines/Fred.chunk'
-    dest_filename1 = 'machines/tmp_1.chunk'
+    dest_filename1 = 'machines/tmp/1.chunk'
     print(f"loading file: {source_filename1}")
     NM1.load_chunks(source_filename1)
     print(f"saving file: {dest_filename1}")
